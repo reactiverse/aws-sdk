60f89f75e171164b2f647facd2dd227741469203
Arnaud ESTEVE <arnaud.esteve@decathlon.com>
2018-11-28 21:52:28 +0100

    add more integration tests (Cloudwatch, S3, Firehose, a bit of gateway)

--- src/test/java/io/vertx/ext/awssdk/integration/LocalStackBaseSpec.java
+++ src/test/java/io/vertx/ext/awssdk/integration/LocalStackBaseSpec.java
@@ -1,11 +1,19 @@
 package io.vertx.ext.awssdk.integration;
 
+import cloud.localstack.docker.LocalstackDocker;
 import io.reactivex.Single;
 import io.reactivex.SingleOnSubscribe;
+import io.vertx.core.Context;
 import software.amazon.awssdk.auth.credentials.AwsCredentials;
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3AsyncClient;
+import software.amazon.awssdk.services.s3.S3AsyncClientBuilder;
 
+import java.net.URI;
 import java.util.concurrent.CompletableFuture;
+
+import static io.vertx.ext.awssdk.VertxSdkClient.withVertx;
 
 public abstract class LocalStackBaseSpec {
 
@@ -20,6 +28,22 @@ public abstract class LocalStackBaseSpec {
             return "a";
         }
     };
+
+    protected static URI s3URI() throws Exception {
+        return new URI(LocalstackDocker.INSTANCE.getEndpointS3());
+    }
+
+    protected static S3AsyncClient s3(Context context) throws Exception {
+        final S3AsyncClientBuilder builder = S3AsyncClient.builder()
+                .serviceConfiguration(sc ->
+                        sc.checksumValidationEnabled(false)
+                                .pathStyleAccessEnabled(true) // from localstack documentation
+                )
+                .credentialsProvider(credentialsProvider)
+                .endpointOverride(s3URI())
+                .region(Region.EU_WEST_1);
+        return withVertx(builder, context).build();
+    }
 
 
     protected static <T> Single<T> single(CompletableFuture<T> future) {
--- src/test/java/io/vertx/ext/awssdk/integration/s3/VertxS3ClientSpec.java
+++ src/test/java/io/vertx/ext/awssdk/integration/s3/VertxS3ClientSpec.java
@@ -1,6 +1,5 @@
 package io.vertx.ext.awssdk.integration.s3;
 
-import cloud.localstack.docker.LocalstackDocker;
 import cloud.localstack.docker.LocalstackDockerExtension;
 import cloud.localstack.docker.annotation.LocalstackDockerProperties;
 import io.vertx.core.Context;
@@ -18,9 +17,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import software.amazon.awssdk.core.async.AsyncRequestBody;
 import software.amazon.awssdk.core.async.AsyncResponseTransformer;
 import software.amazon.awssdk.core.internal.util.Mimetype;
-import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3AsyncClient;
-import software.amazon.awssdk.services.s3.S3AsyncClientBuilder;
 import software.amazon.awssdk.services.s3.model.Bucket;
 import software.amazon.awssdk.services.s3.model.CreateBucketRequest;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
@@ -28,10 +25,8 @@ import software.amazon.awssdk.services.s3.model.ListObjectsRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.model.S3Object;
 
-import java.net.URI;
 import java.util.concurrent.TimeUnit;
 
-import static io.vertx.ext.awssdk.VertxSdkClient.withVertx;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -44,8 +39,8 @@ public class VertxS3ClientSpec extends LocalStackBaseSpec {
     private final static String BUCKET_NAME = "my-vertx-bucket";
     private final static String IMG_FOLDER = "src/test/resources/";
     private final static String RESOURCE_PATH = "s3/cairn_little.jpg";
-    private final static String IMG_PATH = IMG_FOLDER + RESOURCE_PATH;
-    private final static String IMG_NAME = "MyImage";
+    private final static String IMG_LOCAL_PATH = IMG_FOLDER + RESOURCE_PATH;
+    private final static String IMG_S3_NAME = "my-image";
     private final static String ACL = "public-read-write";
     private final static OpenOptions READ_ONLY = new OpenOptions().setRead(true);
 
@@ -72,7 +67,7 @@ public class VertxS3ClientSpec extends LocalStackBaseSpec {
                     assertEquals(1, listRes.buckets().size());
                     final Bucket bucket = listRes.buckets().get(0);
                     assertEquals(BUCKET_NAME, bucket.name());
-                    return rxVertx.fileSystem().rxOpen(IMG_PATH, READ_ONLY);
+                    return rxVertx.fileSystem().rxOpen(IMG_LOCAL_PATH, READ_ONLY);
                 })
                 .flatMap(file -> {
                     final AsyncRequestBody body = AsyncRequestBody.fromPublisher(new ReadStreamPublisher<>(file.getDelegate()));
@@ -88,7 +83,7 @@ public class VertxS3ClientSpec extends LocalStackBaseSpec {
                     assertEquals(1, listRes.contents().size());
                     final S3Object myImg = listRes.contents().get(0);
                     assertNotNull(myImg);
-                    assertEquals(IMG_NAME, myImg.key());
+                    assertEquals(IMG_S3_NAME, myImg.key());
                     return single(s3.getObject(VertxS3ClientSpec::getImg, AsyncResponseTransformer.toBytes()));
                 }).doOnSuccess(getRes -> {
                     assertEquals(originalContext, vertx.getOrCreateContext());
@@ -102,7 +97,7 @@ public class VertxS3ClientSpec extends LocalStackBaseSpec {
 
     private static PutObjectRequest.Builder uploadImg(PutObjectRequest.Builder por) {
         return por.bucket(BUCKET_NAME)
-                .key(IMG_NAME)
+                .key(IMG_S3_NAME)
                 .contentType(Mimetype.MIMETYPE_OCTET_STREAM);
     }
 
@@ -116,20 +111,7 @@ public class VertxS3ClientSpec extends LocalStackBaseSpec {
     }
 
     private static GetObjectRequest.Builder getImg(GetObjectRequest.Builder gor) {
-        return gor.key(IMG_NAME).bucket(BUCKET_NAME);
-    }
-
-    private S3AsyncClient s3(Context context) throws Exception {
-        final URI s3Uri = new URI(LocalstackDocker.INSTANCE.getEndpointS3());
-        final S3AsyncClientBuilder builder = S3AsyncClient.builder()
-                .serviceConfiguration(sc ->
-                        sc.checksumValidationEnabled(false)
-                            .pathStyleAccessEnabled(true) // from localstack documentation
-                )
-                .credentialsProvider(credentialsProvider)
-                .endpointOverride(s3Uri)
-                .region(Region.EU_WEST_1);
-        return withVertx(builder, context).build();
+        return gor.key(IMG_S3_NAME).bucket(BUCKET_NAME);
     }
 
 }
