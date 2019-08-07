72d89688bf6e31e217e280a4546f2a8295938640
Julien Ponge <julien.ponge@gmail.com>
2019-08-07 09:25:57 +0200

    Simplify current Gradle build

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,35 +4,24 @@ val junit5Version = "5.4.0"
 val logbackVersion = "1.2.3"
 val integrationOption = "tests.integration"
 
-buildscript {
-    repositories {
-        jcenter()
-    }
-
-    dependencies {
-        classpath("com.jaredsburrows:gradle-license-plugin:0.8.42")
-    }
-}
-
 plugins {
-    java
-    jacoco
-    maven
     `java-library`
-    id("com.jaredsburrows.license") version("0.8.42")
+    `maven-publish`
+    signing
+    jacoco
+    id("com.jaredsburrows.license") version "0.8.42"
     id("org.sonarqube") version "2.6"
-}
-
-group = "io.reactiverse"
-version = "0.0.1-SNAPSHOT"
-
-tasks.withType<JavaCompile> {
-    sourceCompatibility = JavaVersion.VERSION_1_8.toString()
 }
 
 repositories {
     mavenCentral()
+    maven {
+        url = uri("https://oss.sonatype.org/content/repositories/snapshots")
+    }
 }
+
+group = "io.reactiverse"
+version = "0.0.1-SNAPSHOT"
 
 dependencies {
     api("io.vertx:vertx-core:$vertxVersion")
@@ -48,25 +37,34 @@ dependencies {
     testCompile("org.junit.jupiter:junit-jupiter-engine:$junit5Version")
 }
 
-tasks.withType<Test> {
-    useJUnitPlatform()
-    systemProperties[integrationOption] = System.getProperty(integrationOption)
-    maxParallelForks = 1
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 jacoco {
     toolVersion = "0.8.2"
 }
 
-tasks.jacocoTestReport {
-    dependsOn(":test")
-    reports {
-        xml.isEnabled = true
-        csv.isEnabled = false
-        html.destination = file("$buildDir/jacocoHtml")
+tasks {
+
+    jacocoTestReport {
+        dependsOn(":test")
+        reports {
+            xml.isEnabled = true
+            csv.isEnabled = false
+            html.destination = file("$buildDir/jacocoHtml")
+        }
+    }
+
+    withType<Test> {
+        useJUnitPlatform()
+        systemProperties[integrationOption] = System.getProperty(integrationOption)
+        maxParallelForks = 1
+    }
+
+    withType<Wrapper> {
+        gradleVersion = "5.4.1"
     }
 }
 
-tasks.withType<Wrapper> {
-    gradleVersion = "5.1.1"
-}
