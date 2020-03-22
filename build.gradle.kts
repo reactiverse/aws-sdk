27f3a5cfc0012dc6ad1247055d161b5c0e4bf7c3
Michel Werren <michel.werren@husqvarnagroup.com>
2020-03-22 19:08:23 +0100

    chore: Increase AWS SDK version to most recent.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 val vertxVersion = "4.0.0-milestone4"
-val awsSdkVersion = "2.7.8"
+val awsSdkVersion = "2.10.91"
 val junit5Version = "5.4.0"
 val logbackVersion = "1.2.3"
 val integrationOption = "tests.integration"
