680f0a847402480af600ce3861219188b4c9708d
Arnaud Esteve <aesteve@confluent.io>
2022-02-18 14:09:24 +0100

    Step#2: upgrade localstack

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
-val vertxVersion = "4.0.0"
+val vertxVersion = "4.2.0"
 val awsSdkVersion = "2.15.45"
-val junit5Version = "5.7.0"
+val junit5Version = "5.8.2"
 val logbackVersion = "1.2.3"
 val integrationOption = "tests.integration"
 
@@ -48,7 +48,7 @@ dependencies {
 
     testImplementation("io.vertx:vertx-junit5:$vertxVersion")
     testImplementation("io.vertx:vertx-rx-java2:$vertxVersion")
-    testImplementation("cloud.localstack:localstack-utils:0.2.5")
+    testImplementation("cloud.localstack:localstack-utils:0.2.20")
     testImplementation("ch.qos.logback:logback-classic:$logbackVersion")
     testImplementation("ch.qos.logback:logback-core:$logbackVersion")
     testImplementation("software.amazon.awssdk:aws-sdk-java:$awsSdkVersion")
