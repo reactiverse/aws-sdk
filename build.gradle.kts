6ce9347aa01433a65e93192023214aaa44080b8b
aesteve <arnaud.esteve@gmail.com>
2023-08-31 11:48:12 +0200

    set 1.2.2-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ val localstackVersion = "0.2.22"
 val integrationOption = "tests.integration"
 
 group = "io.reactiverse"
-version = "1.2.2"
+version = "1.2.2-SNAPSHOT"
 
 plugins {
     `java-library`
